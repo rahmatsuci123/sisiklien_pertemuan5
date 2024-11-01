8fc7ce108e13e98dfab8eec8ac546a7f41f369eb
rahmatsuci123 <sulissuci2@gmail.com>
2024-11-01 09:04:12 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-
+import DashboardPage from "./pages/dashboard";
 const App = () => {
   const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <DashboardPage />,
+    },
     {
       path: "/",
       element: <Home />,
---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "../component/layout/MainLayout";
+
+const DashboardPage = () => {
+  return (
+    <MainLayout type="dashboard">
+      <div></div>
+    </MainLayout>
+  );
+};
+
+export default DashboardPage;
